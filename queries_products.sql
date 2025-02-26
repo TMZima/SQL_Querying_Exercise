d716410c0baadfcfc712af3315ed3b95333f5d13
Trey Zima <trey.m.zima@gmail.com>
2025-02-26 15:08:51 -0700

    Completed steps 7 through 10 in products queries

--- queries_products.sql
+++ queries_products.sql
@@ -33,19 +33,22 @@ SELECT name, price FROM products;
 
 -- 7. Add a new product - make up whatever you would like!
 
-
+INSERT INTO products
+    (name, price, can_be_returned)
+VALUES
+    ('laptop', 1299.99, 'f');
 
 -- 8. Display only the products that ***can_be_returned***
 
-
+SELECT * FROM products WHERE can_be_returned;
 
 -- 9. Display only the products that have a price less than 44.00.
 
-
+SELECT * FROM products WHERE price < 44.00;
 
 -- 10. Display only the products that have a price in between 22.50 and 99.99.
 
-
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
 -- 11. There’s a sale going on: Everything is $20 off! Update the database accordingly.
 
