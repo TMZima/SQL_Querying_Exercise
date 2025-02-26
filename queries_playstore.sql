896bd12620027f2b0908fad32a48c6e72bf4a001
Trey Zima <trey.m.zima@gmail.com>
2025-02-26 15:55:36 -0700

    Completed steps 6 through 10 in playstore queries

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -25,23 +25,35 @@ LIMIT 1;
 
 -- 6. Find the average rating for each category ordered by the highest rated to lowest rated.
 
-
+SELECT category, AVG(rating) as avg_rating FROM analytics
+GROUP BY category
+ORDER BY avg_rating DESC;
 
 -- 7. Find the name, price, and rating of the most expensive app with a rating that’s less than 3.
 
-
+SELECT app_name, price, rating FROM analytics
+WHERE rating < 3
+ORDER BY price DESC
+LIMIT 1;
 
 -- 8. Find all apps with a min install not exceeding 50, that have a rating. Order your results by highest rated first.
 
-
+SELECT * FROM analytics
+WHERE min_installs <= 50 
+    AND rating IS NOT NULL
+ORDER BY rating DESC;
 
 -- 9. Find the names of all apps that are rated less than 3 with at least 10000 reviews.
 
-
+SELECT app_name FROM analytics
+WHERE rating < 3 AND reviews >= 10000;
 
 -- 10. Find the top 10 most-reviewed apps that cost between 10 cents and a dollar.
 
-
+SELECT * FROM analytics
+WHERE price BETWEEN 0.1 AND 1
+ORDER BY reviews DESC
+LIMIT 10;
 
 -- 11. Find the most out of date app. Hint: You don’t need to do it this way, but it’s possible to do with a subquery: http://www.postgresqltutorial.com/postgresql-max-function/
 
